d648efefd00111e08e9e6869d4e7ffae7d29e531
momehmandoost <mehmandoost2@gmail.com>
2023-11-23 17:31:23 +0330

    Add color to CTA banner

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -93,7 +93,7 @@ function Featured() {
       <div className="featured-cta flex justify-center text-left items-center md1000:bg-none md1000:bg-[#FAFF00] ">
         <div className="container page-padding ">
           <div className="flex items-center md1000:flex-col md1000:gap-12  md1000:text-center">
-            <h2 className="text-white font-bold text-[3.7rem] md1000:text-[3rem] max-w-6xl px-7 leading-[1.2] min450:text-[2.4rem] ">
+            <h2 className="text-white font-bold text-[3.7rem] md1000:text-black md1000:text-[3rem] max-w-6xl px-7 leading-[1.2] min450:text-[2.4rem] ">
               We Are Always Providing Best Fitness Service For You
             </h2>
             <MainButton
